0a21241fa32b1c5b4d90ff0e128737499d85c7fc
HasaanMaster <hmaster042020@gmail.com>
2026-09-16 23:46:21 -0500

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False 
+        sMap = {}
+        for i in range(len(s)):
+            sMap[s[i]] = sMap.get(s[i],0) + 1
+        tMap = {}
+        for j in range(len(t)):
+            tMap[t[j]] = tMap.get(t[j], 0 ) + 1
+        if sMap != tMap:
+            return False 
+        return True  
